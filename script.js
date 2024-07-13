d2f0073521f996bfcc9b2487ec817fef91fbcc06
sandileZ <sandizikz@gmail.com>
2024-07-13 23:31:37 +0200

    added navigation including hamburger

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello!');
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
